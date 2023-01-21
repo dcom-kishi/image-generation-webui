6e2efc489c3d12d2ee248d3491264fce005727cb
kishi.yusuke <84057185+dcom-kishi@users.noreply.github.com>
2023-01-21 17:06:13 +0900

    feat: Add minimal frontend and backend environment (#1)
    * feat : Setup RESTful API environment
    * feat : Setup react environment

---
+++ backend/app.js
@@ -0,0 +1,50 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// rest api route
+app.use('/api/v1/users', usersRouter);
+
+// use react resource.
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// use react index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+module.exports = app;
